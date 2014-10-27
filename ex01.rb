21594554601f96a4aa26ef00f973f4ec4146ce6e
elan690 <elan1179@gmail.com>
2014-10-27 20:42:01 +0800

    build files

---
+++ ex01.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! printing."
+puts "I'd must rather you 'not'."
+puts 'I "said do not touch this'
+
