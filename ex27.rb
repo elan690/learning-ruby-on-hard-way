f373031d2615f1c1b9c3a9dba814b6bba9130ccc
elan690 <elan1179@gmail.com>
2014-11-10 00:56:08 +0800

    finish Ruby26-30

---
+++ ex27.rb
@@ -0,0 +1,26 @@
+puts !false
+puts !true
+puts true||false
+puts true||true
+puts false||true
+puts false||false
+
+puts  true&&false
+puts  true&&true
+puts false&&true
+puts false&&false
+
+puts !(true&&false)
+puts !(true&&true)
+puts !(false&&true)
+puts !(false&&false)
+
+puts 1!=0
+puts 1!=1
+puts 0!=1
+puts 0!=0
+
+puts 1==0
+puts 1==1
+puts 0==1
+puts 0==0
---
+++ ex28.rb
@@ -0,0 +1,60 @@
+    puts true and true
+    true
+
+    puts false and true
+    false
+
+    puts1 == 1 and 2 == 1
+    true
+
+    puts "test" == "test"
+    true
+
+    puts 1 == 1 or 2 != 1
+     true
+     
+    puts true and 1 == 1
+     true
+     
+    puts false and 0 != 0
+     false
+
+     puts true or 1 == 1
+     true
+    
+     puts  "test" == "testing"
+     false
+     
+     puts 1 != 0 and 2 == 1
+     false
+     
+     puts "test" != "testing"
+     true
+     
+     puts "test" == 1
+     false
+
+     puts not (true and false)
+     true
+
+     puts not (1 == 1 and 0 != 1)
+     false
+
+     puts not (10 == 1 or 1000 == 1000)
+     false
+     
+     puts not (1 != 10 or 3 == 4)
+     false
+     
+     puts not ("testing" == "testing" and "Zed" == "Cool Guy")
+     true
+    
+    puts  1 == 1 and not ("testing" == 1 or 1 == 0)
+     true
+     
+     puts "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+     false
+     
+     puts 3 == 3 and not ("testing" == "testing" or "Ruby" == "Fun")
+     false
+
